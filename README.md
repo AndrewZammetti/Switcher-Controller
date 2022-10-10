6a4e1e0ea9259250afdbc09be1adb8c2e6d814ad
Andrew Zammetti <azamm235@live.kutztown.edu>
2022-10-10 14:01:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Switcher-Controller
+ A controller custom designed for WEEU's ACS 8.2 Switcher
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+
+using namespace std;
+
+int main(){
+    HANDLE serialHandle = CreateFile((LPCWSTR)"COM1", GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+    if(serialHandle==INVALID_HANDLE_VALUE){
+        if(GetLastError() == ERROR_FILE_NOT_FOUND){
+            cout<<"No Serial device found!\n";
+        }
+        cout<<"Undefined error!\n";
+    }
+
+    DCB dcbSerialParam = {0};
+    dcbSerialParam.DCBlength = sizeof(dcbSerialParam);
+
+    if (!GetCommState(serialHandle, &dcbSerialParam)) {
+        cout<<"Couldn't get comm state!\n";
+    }
+
+    dcbSerialParam.BaudRate=CBR_9600;
+    dcbSerialParam.ByteSize=8;
+    dcbSerialParam.StopBits=ONESTOPBIT;
+    dcbSerialParam.Parity=NOPARITY;
+
+    if(!SetCommState(serialHandle, &dcbSerialParam)){
+        cout<<"Couldn't set comm state!\n";
+    }
+
+    
+}
